62749ead9f9cd4b64ae1bb3a064192a9baedd486
Sourav Satyam <sourav.satyamsingh@gmail.com>
2024-08-19 23:38:55 +0530

    Added the config settings for development & production

--- config/config.js
+++ config/config.js
@@ -7,18 +7,18 @@ const config = {
     "dialect": "postgres"
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": process.env.PGUSER,
+    "password": process.env.PGPASSWORD,
+    "database": process.env.PGDATABASE,
+    "host": process.env.PGHOST,
+    "dialect": "postgres"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": process.env.PGUSER,
+    "password": process.env.PGPASSWORD,
+    "database": process.env.PGDATABASE,
+    "host": process.env.PGHOST,
+    "dialect": "postgres"
   }
 }
 
